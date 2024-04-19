3cae5958d69555043cd972a448deab3ea383fafb
zjeee <wiritipon.t@ku.th>
2024-04-19 20:32:29 +0700

    add new file struct

---
+++ client/src/contents/header.js
@@ -0,0 +1,7 @@
+export const data = {
+    name:"Wiritipon Thapkhan",
+    title:"Computer Engineering Student",
+    caption:"“If debugging is the process of removing software bugs, then programming must be the process of putting them in.”",
+    btnText:"View Resume",
+    link:""
+}
--- client/src/sections/Rightsection/index.jsx
+++ client/src/sections/Rightsection/index.jsx
@@ -1,7 +1,9 @@
+import About from "../About";
+
 const Rightsection = () => {
     return (
-        <div>
-            <div>About</div>
+        <div className="grid gap-y-4">
+            <About/>
             <div className='mb-96'>Exp</div>
             <div className='mb-96'>Exp</div>
             <div className='mb-96'>Exp</div>
